f486d0a7c28587a3c15958267bd343fb69c2a239
Ayushi <msseema007@gmail.com>
2024-07-30 15:59:30 +0530

    NODEJS

--- auto_increment.js
+++ auto_increment.js
@@ -19,5 +19,22 @@ con.connect((err) => {
 })
 
 app.post("/post",(req,res)=>{
-    
+    const name=req.body.name;
+    const mark=req.body.mark;
+    con.query('insert into mytable(name,mark) values(?,?)',[name,mark],(err,result)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("POSTED!!");
+        }
+    })
+})
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("on port 3000!")
+    }
+
 })
